a91a23c759427e80331ed446122a048ba499ad83
fscali <francesco.scali@gmail.com>
2019-12-22 08:39:53 +0100

    Cache as a data storage

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,30 +1,28 @@
 @extends('layout')
 
 @section('content')
-    <h1>
-        {{ $post->title }}
-        @badge(['show' => now()->diffInMinutes($post->created_at) < 30])
-            Brand new Post!
-        @endbadge
-    </h1>
+<h1>
+    {{ $post->title }}
+    @badge(['show' => now()->diffInMinutes($post->created_at) < 30]) Brand new Post! @endbadge </h1> <p>
+        {{ $post->content }}</p>
 
-    <p>{{ $post->content }}</p>
-
-    @updated(['date' => $post->created_at, 'name' => $post->user->name])
-    @endupdated
-    @updated(['date' => $post->updated_at])
+        @updated(['date' => $post->created_at, 'name' => $post->user->name])
+        @endupdated
+        @updated(['date' => $post->updated_at])
         Updated
-    @endupdated
+        @endupdated
 
-    <h4>Comments</h4>
+        <p>Currently read by {{ $counter }} people</p>
 
-    @forelse($post->comments as $comment)
+        <h4>Comments</h4>
+
+        @forelse($post->comments as $comment)
         <p>
             {{ $comment->content }}
         </p>
         @updated(['date' => $comment->created_at])
         @endupdated
-    @empty
+        @empty
         <p>No comments yet!</p>
-    @endforelse
-@endsection('content')
+        @endforelse
+        @endsection('content')
