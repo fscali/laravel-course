6a516f68579258c42f3549fdabb8f4d75d83994e
fscali <francesco.scali@gmail.com>
2019-12-28 12:39:02 +0100

    ..

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,8 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
         'App\BlogPost' => 'App\Policies\BlogPostPolicy',
+        'App\User' => 'App\Policies\UserPolicy',
+
     ];
 
     /**
